fcdb0953ed79e5f38b6f7ecd5fd94bc628753820
Rahul Ramesh <121226043+rahu1ramesh@users.noreply.github.com>
2024-08-27 10:18:20 +0530

    BAH-3884 | Fix. Bump Dependencies With Vulnerabilities (#97)

--- src/main/java/org/bahmni/reports/util/SqlUtil.java
+++ src/main/java/org/bahmni/reports/util/SqlUtil.java
@@ -2,7 +2,7 @@ package org.bahmni.reports.util;
 
 import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringEscapeUtils;
+import org.springframework.util.StringUtils;
 import org.quartz.impl.jdbcjobstore.InvalidConfigurationException;
 
 import java.sql.Connection;
@@ -13,13 +13,14 @@ import java.sql.Statement;
 import java.util.List;
 
 public class SqlUtil {
+
     public static String toEscapedCommaSeparatedSqlString(List<String> list) {
         if (CollectionUtils.isEmpty(list)) {
             return "";
         }
         StringBuilder sb = new StringBuilder();
         for (String item : list) {
-            sb.append("\\'").append(StringEscapeUtils.escapeSql(item)).append("\\',");
+            sb.append("\\'").append(escapeSql(item, true, null)).append("\\',");
         }
         return sb.substring(0, sb.length() - 1);
     }
@@ -30,7 +31,7 @@ public class SqlUtil {
         }
         StringBuilder sb = new StringBuilder();
         for (String item : list) {
-            sb.append("'").append(StringEscapeUtils.escapeSql(item)).append("',");
+            sb.append("'").append(escapeSql(item, true, null)).append("',");
         }
         return sb.substring(0, sb.length() - 1);
     }
@@ -84,6 +85,61 @@ public class SqlUtil {
         }
 
         return null;
+    }
+
+    public static String escapeSql(String str, boolean escapeDoubleQuotes, Character escapeChar) {
+        if (StringUtils.isEmpty(str)) {
+            return str;
+        }
+        char escChar = '\\';
+        if (escapeChar != null) {
+            escChar = escapeChar.charValue();
+        }
+        String strToCheck = str.trim().replace("0x", "0X").replace("/*", "\\/*");
+        StringBuilder sBuilder = new StringBuilder();
+        int stringLength = strToCheck.length();
+        for (int i = 0; i < stringLength; ++i) {
+            char c = strToCheck.charAt(i);
+            switch (c) {
+                case 0:
+                    sBuilder.append(escChar);
+                    sBuilder.append('0');
+                    break;
+                case ';':
+                    sBuilder.append(escChar);
+                    sBuilder.append(';');
+                    break;
+                case '\n': /* Must be escaped for logs */
+                    sBuilder.append(escChar);
+                    sBuilder.append('n');
+                    break;
+                case '\r':
+                    sBuilder.append(escChar);
+                    sBuilder.append('r');
+                    break;
+                case '\\':
+                    sBuilder.append(escChar);
+                    sBuilder.append('\\');
+                    break;
+                case '\'':
+                    sBuilder.append(escChar);
+                    sBuilder.append('\'');
+                    break;
+                case '"':
+                    if (escapeDoubleQuotes) {
+                        sBuilder.append('\\');
+                    }
+                    sBuilder.append('"');
+                    break;
+                case '\032':
+                    sBuilder.append(escChar);
+                    sBuilder.append('Z');
+                    break;
+                default:
+                    sBuilder.append(c);
+            }
+        }
+        return sBuilder.toString();
     }
 }
 
